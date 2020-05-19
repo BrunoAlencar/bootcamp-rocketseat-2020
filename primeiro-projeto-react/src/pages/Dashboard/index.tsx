c0e22e43ab012d8f849852c48dfeb672a68dcfc2
Bruno Alencar <joaobrunoalencar@gmail.com>
2020-05-19 13:25:43 -0300

    Added axios and create api service with github api url

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,46 +1,39 @@
-import React from 'react';
-
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { api } from '../../services/api';
+
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepository] = useState([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+    console.log(response.data);
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="#a">
-          <img
-            src="https://avatars0.githubusercontent.com/u/11480376?s=460&u=249d0d0419f27e0e5915c40b0f4c9a538232ddd9&v=4"
-            alt="Bruno Alencar"
-          />
-          <div>
-            <strong>github/brunoalencar</strong>
-            <p>Easy peasy lemon squeezy</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#a">
-          <img
-            src="https://avatars0.githubusercontent.com/u/11480376?s=460&u=249d0d0419f27e0e5915c40b0f4c9a538232ddd9&v=4"
-            alt="Bruno Alencar"
-          />
-          <div>
-            <strong>github/brunoalencar</strong>
-            <p>Easy peasy lemon squeezy</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
         <a href="#a">
           <img
             src="https://avatars0.githubusercontent.com/u/11480376?s=460&u=249d0d0419f27e0e5915c40b0f4c9a538232ddd9&v=4"
