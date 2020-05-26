8ae2f51576da14de90376709e4e2584eda22e6f8
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2020-05-26 09:31:41 -0300

    Created signup page and add lib of unform

---
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FiArrowLeft, FiLogIn, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" Icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" Icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            Icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
