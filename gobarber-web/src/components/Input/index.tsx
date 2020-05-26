2d9eda3e7838dc42a94d2fea35654eb367913fd1
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2020-05-25 21:05:41 -0300

    Created login page with isolated components of button and input to reuse it in anothers pages

---
+++ gobarber-web/src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  Icon: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ Icon, ...rest }) => (
+  <Container>
+    {Icon && <Icon size={20} />}
+    <input {...rest} type="text" />
+  </Container>
+);
+
+export default Input;
